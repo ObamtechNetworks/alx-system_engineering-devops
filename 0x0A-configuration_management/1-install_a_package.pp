5dc2d262ba75e452a726147df1062f76d5276805
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-01-21 21:00:25 +0100

    install flask with puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,23 @@
+# a puppet manifest to install `flask` from `pip3`
+# Requirements `flask` version must be `2.1.0`
+
+# Define package and version 
+$package_name = 'flask'
+$version = '2.1.0'
+
+
+# install werkzeug
+$werkzeug_package = 'werkzeug'
+$werkzeug_version = '2.1.0'
+
+# install flask with the specified version
+package { $package_name:
+  ensure   => $version,
+  provider => 'pip3',
+}
+
+# install the werkzeug
+package { $werkzeug_package:
+  ensure   => $werkzeug_version,
+  provider => 'pip3',
+}
