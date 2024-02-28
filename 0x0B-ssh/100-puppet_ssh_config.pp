04e84823ea4dd6db0594586c34ef5469f6930a62
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-29 00:10:44 +0100

    maybe this but tis crazy

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,12 @@
-# ssh_config.pp
+# make changes to our configuration file with puppet
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
 
-# Ensure SSH client configuration
-file { '/home/ubuntu/.ssh/config':
-  ensure => present,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0600',
-  content => '
-    Host ubuntu
-      HostName 3.85.41.84
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ',
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
