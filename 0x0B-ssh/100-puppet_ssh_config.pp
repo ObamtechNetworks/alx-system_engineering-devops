c6a0a705c7b2c63617245593edec5a8b2d2d2a93
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-28 23:50:37 +0100

    get home directory dynamically

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,17 @@
 # using puppet to run ssh client configuration
 
-$private_key = '~/.ssh/school'
+# Get the home directory of the current user
+$user_home = $facts['os']['family'] ? {
+  'windows' => $facts['env']['USERPROFILE'],
+  default   => $facts['env']['HOME'],
+}
+
+# Define variables for the private key path and passphrase authentication
+$private_key = "${user_home}/.ssh/school"
 $passkey_auth = 'no'
 
-# execute command
-file { '/home/ubuntu/.ssh/config':
-  ensure => 'file',
-  content => "Host mypupet_config\n\tHostName 54.237.76.176\n\tUser ubuntu\n\tIdentityFile ${private_key}\n\tPasswordAuthentication ${passkey_auth}\n"
+# Execute command to create or modify the SSH client configuration file
+file { "${user_home}/.ssh/config":
+  ensure  => 'file',  # Ensure that the file exists
+  content => "Host mypuppet_config\n\tHostName 54.237.76.176\n\tUser ubuntu\n\tIdentityFile ${private_key}\n\tPasswordAuthentication ${passkey_auth}\n"
 }
