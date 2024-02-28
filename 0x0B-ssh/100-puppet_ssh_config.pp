5a0b4ecce6dc42444446243027469fe0d0c7cfcc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-29 00:03:17 +0100

    almost

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,10 @@
 # using puppet to run ssh client configuration
 
-# Get the home directory of the current user
-$user_home = $facts['os']['family'] ? {
-  'windows' => $facts['env']['USERPROFILE'],
-  default   => $facts['env']['HOME'],
-  undef     => '/default/path',  # Provide a default path in case facts are not available
-}
-
-# Define variables for the private key path and passphrase authentication
-$private_key = "${user_home}/.ssh/school"
+$private_key = '~/.ssh/school'
 $passkey_auth = 'no'
 
-# Execute command to create or modify the SSH client configuration file
-file { "${user_home}/.ssh/config":
-  ensure  => 'file',  # Ensure that the file exists
-  content => "Host mypuppet_config\n\tHostName 54.237.76.176\n\tUser ubuntu\n\tIdentityFile ${private_key}\n\tPasswordAuthentication ${passkey_auth}\n"
+# execute command
+file { '/home/ubuntu/.ssh/config':
+  ensure => 'file',
+  content => "Host mypupet_config\n\tHostName 54.237.76.176\n\tUser ubuntu\n\tIdentityFile ${private_key}\n\tPasswordAuthentication ${passkey_auth}\n"
 }
