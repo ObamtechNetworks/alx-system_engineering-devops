0290c0d06ea714a86bc1cfaf0a5063c3b2e9af44
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-03-02 23:01:52 +0100

    use puppet to configure nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,49 @@
+# puppet code to configure an nginx server
+# install nginx 
+
+package {'nginx':
+  ensure => installed,
+}
+
+# customize content of the index.html
+file { '/var/www/html/index.html':
+  ensure => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Configure Nginx server block
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+
+  root /var/www/html;
+  index index.html index.htm;
+
+  server_name obam.tech www.obam.tech;
+
+  location / {
+    try_files $uri $uri/ =404;
+  }
+
+  location = /redirect_me/ {
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  }
+
+  location = /redirect_me {
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  }
+
+  error_page 404 /404.html;
+}',
+  require => Package['nginx'],
+}
+
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
