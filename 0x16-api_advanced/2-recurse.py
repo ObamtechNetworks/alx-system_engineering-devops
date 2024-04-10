ba19b154446618d8e187b7379264d38ff32ebd2e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-04-10 03:27:27 +0100

    reconfigure for fake redit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -16,24 +16,30 @@ def recurse(subreddit, hot_list=[], after=None):
     if after:
         params["after"] = after
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
 
     if response.status_code == 404:
         return None  # Subreddit not found
 
-    data = response.json()
+    try:
+        data = response.json()
 
-    if "data" not in data or "children" not in data["data"]:
-        return None  # Handle unexpected API response structure
+        if "data" not in data or "children" not in data["data"]:
+            return None  # Handle unexpected API response structure
 
-    after = data["data"].get("after")
-    posts = data["data"]["children"]
+        after = data["data"].get("after")
+        posts = data["data"]["children"]
 
-    for post in posts:
-        title = post["data"]["title"]
-        hot_list.append(title)
+        for post in posts:
+            title = post["data"]["title"]
+            hot_list.append(title)
 
-    if after:
-        recurse(subreddit, hot_list, after)  # Recursive call for next page
+        if after:
+            recurse(subreddit, hot_list, after)  # Recursive next pg
+    except Exception as e:
+        return None
 
     return hot_list
