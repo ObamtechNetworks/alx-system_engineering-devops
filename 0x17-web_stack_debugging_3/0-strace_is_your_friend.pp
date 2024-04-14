5a339d21a5f8b711e234d9ca6bdb98f2429fdbe1
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-04-15 00:06:13 +0100

    changed file path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to ensure the occurrence of a 500 error is resolved and add specified requirements
 
-exec {'replace':
- provider => shell,
- command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  content => file('/var/www/html/wp-settings.php').content.replace('phpp', 'php')
 }
