444b41edd9833180685725a838c8d41bf7594804
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-04-15 00:09:27 +0100

    fixed puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Puppet manifest to ensure the occurrence of a 500 error is resolved and add specified requirements
+# Resolve 500 error
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.replace('phpp', 'php')
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
