e2a06345329f74f198e754a914de1b8cbfd0d053
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-04-15 00:00:12 +0100

    added comment first line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Puppet manifest to ensure the occurrence of a 500 error is resolved and add specified requirements
+
 file { '/var/www/html/wp-settings.php':
   ensure  => file,
   content => file('/var/www/html/wp-settings.php')
